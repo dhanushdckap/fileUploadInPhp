084d9f269f732c12a9cb5948105db643b44734ad
DCKAP-L150 <dhanushgdckap@gmail.com>
2023-06-21 12:43:41 +0530

    file upload task completed

---
+++ index.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html>
+<head>
+<title>File Upload</title>
+</head>
+<body>
+<form action="upload.php" method="post" enctype="multipart/form-data">
+    <input type="text" name="userName" />
+    <input type="file" name="file" />
+    <button type="submit" name="upload">upload</button>
+</form>
+</body>
+</html>
